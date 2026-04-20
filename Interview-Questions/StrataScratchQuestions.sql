71e931cab9d795f14774e32e42fdc0b29c47e68b
moizalikhan <moizalikhan215@gmail.com>
2026-04-20 14:48:34 +0500

    2026-04-20

--- Interview-Questions/StrataScratchQuestions.sql
+++ Interview-Questions/StrataScratchQuestions.sql
@@ -1056,3 +1056,15 @@ where ST.ranking = IT.ranking+1
 AND (ST.created_at::date - IT.created_at::date) <= 7
 AND (ST.created_at::date - IT.created_at::date) >= 1
 order by IT.user_id ASC
+--------------------------------------
+-- ID 9650
+-- Find the top 10 ranked songs in 2010.
+-- Output the rank, group name, and song name,
+-- but do not show the same song twice.
+-- Sort the result based on the rank in ascending order.
+-- Table
+-- billboard_top_100_year_end
+select distinct year_rank,group_name,song_name from billboard_top_100_year_end
+where year='2010'
+order by year_rank ASC
+limit 10;
